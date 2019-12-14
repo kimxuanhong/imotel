15b89bea26c523a3ef512e809dd7026dc1e29825
Xuan Hong <kimxuanhong@outlook.com>
2019-12-15 02:23:35 +0700

    Updates

---
+++ precache-manifest.b3bb7410b15de22f7845d3d32439b203.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f7259abe7870a75b29b3c720a62e12d",
+    "url": "/imotel/index.html"
+  },
+  {
+    "revision": "0187e4e15b419f599995",
+    "url": "/imotel/static/css/main.8ab6f918.chunk.css"
+  },
+  {
+    "revision": "b2feed363e8c05573db9",
+    "url": "/imotel/static/js/2.6e4c4941.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/imotel/static/js/2.6e4c4941.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0187e4e15b419f599995",
+    "url": "/imotel/static/js/main.f971b107.chunk.js"
+  },
+  {
+    "revision": "073379b76ea43461cb89",
+    "url": "/imotel/static/js/runtime-main.dd66ca0a.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/imotel/static/media/logo.ee7cd8ed.svg"
+  }
+]);
